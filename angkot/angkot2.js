787c2fc32a299c968f843e5038f06399ad9f1607
veralindha <30642445+veralindha@users.noreply.github.com>
2020-08-02 10:07:28 +0700

    Add files via upload

---
+++ angkot/angkot2.js
@@ -0,0 +1,61 @@
+var penumpang = [];
+var tambahPenumpang = function(namaPenumpang , penumpang){
+    //jika angkot kosong
+    if( penumpang.length == 0){
+    //tambah penumpang diawal array
+        penumpang.push(namaPenumpang);
+    // kembalikan isi array
+        return penumpang;
+    }//jika tidak kosong
+    else{
+     //telusuri seluruh kursi dari awal
+        for(var i = 0;i < penumpang.length; i++){
+        //jika ada kursi kosong
+            if(penumpang[i] == undefined){
+            //tambah penumpang di kursi tersebut
+                penumpang[i] = namaPenumpang;
+            //kembalikan isi array
+                return penumpang;
+            }
+        //jika ada nama penumpang yang sama
+            else if(penumpang[i] == namaPenumpang){
+            //tampilkan pesan kesalahan
+                console.log(namaPenumpang + ' sudah ada didalam angkot. ');
+            //kemabalikan isi array
+                return penumpang;
+            }
+        //jika seluruh kursi terisi
+            else if( i == penumpang.length - 1){
+            //tambahkan penumpang di akhir array
+                penumpang.push(namaPenumpang);
+            //kembalikan isi array
+                return penumpang;
+            }
+        }
+    }
+}
+
+var hapusPenumpang = function(namaPenumpang , penumpang){
+    //jika angkot kosong
+    if(penumpang.length == 0){
+        //tampilkan pesan bahwa angkot kosong
+        console.log('angkot masih kosong.');
+    }
+    //jika angkot tidak kosong
+    else{
+        //telusuri seluruh kursi dari awal
+        for(var i = 0; i < penumpang.length; i++){
+            //jika nama penumpang sesuai
+            if( penumpang[i] == namaPenumpang){
+                //hapus penumpang & ubah namanya menjadi undefined
+                penumpang[i] = undefined;
+            }
+            //jika tidak ada nama yang sesuai
+            else if(i == penumpang.length - 1){
+                //tampilkan pesan kesalahan
+                console.log(namaPenumpang + ' tidak ada didalam angkot.');
+            }
+        }
+    }
+    return penumpang;
+}
